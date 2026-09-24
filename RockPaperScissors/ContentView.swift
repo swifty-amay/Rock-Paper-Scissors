6e81c61bae883c269b1a47033ccddcd0de5f527d
swifty-amay <rajamay9304972983@gmail.com>
2026-09-24 19:34:50 +0530

    Implement game logic successfully

--- RockPaperScissors/ContentView.swift
+++ RockPaperScissors/ContentView.swift
@@ -9,33 +9,81 @@ import SwiftUI
 
 struct ButtonView: View {
     let name: String
+    let completion: () -> Void
     var body: some View {
-        Text(name)
+        Button(name, action: completion)
+            .buttonStyle(.borderedProminent)
+            .tint(.teal)
+            .font(.system(size: 20))
     }
 }
-
-
 
 struct ContentView: View {
     
     let items = ["🪨", "📰", "✂️"]
     @State private var appsMove = 0
     @State private var playerChoice = false
+    @State private var score = 0
     
     var body: some View {
         VStack{
-            Text("Score: ???")
+            Text("Score: \(score)")
                 .font(.largeTitle.bold())
             Text("It's a: \(playerChoice ? "Win" : "Lose")")
                 .font(.title)
             Text(items[appsMove])
                 .font(.system(size: 200))
             HStack(spacing: 48){
-                ButtonView(name: "Rock")
-                ButtonView(name: "Paper")
-                ButtonView(name: "Scissor")
+                ButtonView(name: "Rock"){
+                    checkAnswer(for: items[appsMove], value: playerChoice, with: "rock")
+                }
+                ButtonView(name: "Paper"){
+                    checkAnswer(for: items[appsMove], value: playerChoice, with: "paper")
+
+                }
+                ButtonView(name: "Scissor"){
+                    checkAnswer(for: items[appsMove], value: playerChoice, with: "scissor")
+
+                }
             }
         }
+    }
+    
+    func askAgain(){
+        appsMove = Int.random(in: 0..<3)
+        playerChoice = Bool.random()
+    }
+    
+    func checkAnswer(for appMove: String, value playerChoice: Bool, with playerMove: String){
+        switch appMove{
+        case "🪨":
+            if(playerChoice && playerMove == "paper"){
+                score += 1
+            } else if(!playerChoice && playerMove == "scissor"){
+                score += 1
+            } else{
+                score -= 1
+            }
+        case "✂️":
+            if(playerChoice && playerMove == "rock"){
+                score += 1
+            } else if(!playerChoice && playerMove == "paper"){
+                score += 1
+            } else{
+                score -= 1
+            }
+        case "📰":
+            if(playerChoice && playerMove == "scissor"){
+                score += 1
+            } else if(!playerChoice && playerMove == "rock"){
+                score += 1
+            } else{
+                score -= 1
+            }
+        default:
+            score = 0
+        }
+        askAgain()
     }
 }
 
